8660a8d416571182b5d87285b38e8f09f2663627
samme1210 <samme1210@gmail.com>
2023-03-05 14:16:53 +0000

    fixed the onloadRecipies() function now works universally on website and streamlined code by a considerable amount

--- app.js
+++ app.js
@@ -3,21 +3,48 @@ const apiKey = "apiKey=f0be80267acc4b1da34a6b913d0918a8";
 const queryURL = "https://api.spoonacular.com/recipes/";
 window.onload = onloadRecipies();
 
+/*
+main function fetches requested data from spoonacular api transforms the promise into readable data, 
+the uses this data to amend the html accordingly 
+*/
+
 async function main(input) {
-    try {
-        const recipies = await fetch(queryURL + input + "&number=6&addRecipeInformation=true");
-        if (!recipies.ok) {
-            throw new Error('Error retrieving recipe data');
-        }
-        const recipeData = await recipies.json();
-        console.log(recipeData)
-        recipeResults.innerHTML = (recipeData.results || recipeData.recipes).map((recipe) => recipeHTML(recipe)).join("");
-    } catch (error) {
-        console.error(error);
-        console.log(recipeData);
-        recipes.innerHTML = '<div class="error">An error occurred while retrieving recipe data.</div>'
-    }
+    console.log(input)
+    const recipies = await fetch(queryURL + input + "&number=6&addRecipeInformation=true");
+    const recipeData = await recipies.json();
+    console.log(recipies)
+    console.log(recipeData)
+    recipeResults.innerHTML = (recipeData.results || recipeData.recipes).map((recipe) => recipeHTML(recipe)).join("");
 }
+
+/*
+on load of food.html function will load recipes based on the user input from either index.html or food.html, 
+otherwise load random rercipes in the event of no user input from index.html
+*/
+
+function onloadRecipies() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userInput = urlParams.get("userInput"); 
+    let searchType;
+
+    if (userInput) {
+        searchType = "complexSearch?";
+    } else {
+        searchType = "random?";
+    }
+
+    const input = searchType + apiKey + (userInput ? `&query=${userInput}` : '');
+    main(input);
+}
+
+function recipeFilter() {
+    const cardThree = '#recipes div:nth-child(3)'
+    console.log(cardThree)
+    const targetCardThree = document.querySelector(cardThree)
+    const filterModalActivate = targetCardThree.classList += ' .filter__modal'
+    return filterModalActivate
+}
+
 
 function recipeHTML(recipe) {
     return `<div class="recipe__card--wrapper">
@@ -52,41 +79,6 @@ function recipeHTML(recipe) {
     </div>`
 }
 
-//on load of food.html function will load recipes based on the user input from index.html 
-//or load random rercipes in the event of no user input from index.html
-
-function onloadRecipies() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userInput = urlParams.get('userInput');
-    const queryType = '&query=' + userInput;
-    let searchType;
-
-    if (!userInput) {
-        searchType = "complexSearch?";
-        let input = searchType + apiKey + queryType;
-        main(input);
-    } else {
-        searchType = "random?";
-        let input = searchType + apiKey;
-        main(input);
-    }
-}
-
-function getFoodInput() {
-    const userInput = document.querySelector('input').value
-    const searchType = "complexSearch?";
-    const queryType = `&query=${userInput}`;
-    let input = searchType + apiKey + queryType;
-    main(input)
-}
-
-function recipeFilter() {
-    const cardThree = '#recipes div:nth-child(3)'
-    console.log(cardThree)
-    const targetCardThree = document.querySelector(cardThree)
-    const filterModalActivate = targetCardThree.classList += ' .filter__modal'
-    return filterModalActivate
-}
 
 //function triggerLoadingState(event) {
 //    const 
