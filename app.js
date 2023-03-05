99a0b4b46e70ebbb578deef9916b60c145dc5423
samme1210 <samme1210@gmail.com>
2023-03-05 11:21:59 +0000

    added onload recipies function to streamline my previous functions

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const recipeResults = document.querySelector('.recipes');
+const recipeResults = document.querySelector('#recipes');
 const apiKey = "apiKey=f0be80267acc4b1da34a6b913d0918a8";
 const queryURL = "https://api.spoonacular.com/recipes/";
+window.onload = onloadRecipies();
 
 async function main(input) {
     try {
-        const recipies = await fetch(queryURL + input + `&number=6&addRecipeInformation=true`);
+        const recipies = await fetch(queryURL + input + "&number=6&addRecipeInformation=true");
         if (!recipies.ok) {
             throw new Error('Error retrieving recipe data');
         }
@@ -13,6 +14,7 @@ async function main(input) {
         recipeResults.innerHTML = (recipeData.results || recipeData.recipes).map((recipe) => recipeHTML(recipe)).join("");
     } catch (error) {
         console.error(error);
+        console.log(recipeData);
         recipes.innerHTML = '<div class="error">An error occurred while retrieving recipe data.</div>'
     }
 }
@@ -50,26 +52,44 @@ function recipeHTML(recipe) {
     </div>`
 }
 
-function getRandomRecipies() {
-    const searchType = "random?";
-    let input = searchType + apiKey;
-    main(input)
+//on load of food.html function will load recipes based on the user input from index.html 
+//or load random rercipes in the event of no user input from index.html
+
+function onloadRecipies() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userInput = urlParams.get('userInput');
+    const queryType = '&query=' + userInput;
+    let searchType;
+
+    if (!userInput) {
+        searchType = "complexSearch?";
+        let input = searchType + apiKey + queryType;
+        main(input);
+    } else {
+        searchType = "random?";
+        let input = searchType + apiKey;
+        main(input);
+    }
 }
 
-getRandomRecipies()
-
-function getUserInput() {
+function getFoodInput() {
     const userInput = document.querySelector('input').value
     const searchType = "complexSearch?";
     const queryType = `&query=${userInput}`;
     let input = searchType + apiKey + queryType;
     main(input)
-} 
+}
 
 function recipeFilter() {
-    const targetModalCard = document.querySelector('#recipes :nth-child(3)')
-    const filterModalActivate = targetModalCard.classList += ' .filter__modal'
+    const cardThree = '#recipes div:nth-child(3)'
+    console.log(cardThree)
+    const targetCardThree = document.querySelector(cardThree)
+    const filterModalActivate = targetCardThree.classList += ' .filter__modal'
     return filterModalActivate
 }
 
+//function triggerLoadingState(event) {
+//    const 
+//}
+//
 //onclick="location.href='${recipe.sourceUrl}'
