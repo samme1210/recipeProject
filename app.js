e474714d4f7bf0adab3dd411c510d73918f19a97
samme1210 <samme1210@gmail.com>
2023-03-21 23:43:04 +0000

    working on contact modal

--- app.js
+++ app.js
@@ -110,3 +110,35 @@ function refine(input) {
 function toggleMenu() {
     document.body.classList.toggle("toggled");
 }
+
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function contact(event) {
+   event.preventDefault();
+   const loading = document.querySelector('.modal__overlay--loading');  
+   const success = document.querySelector('.modal__overlay--success');
+   loading.classList.add("modal__overlay--visible");
+    emailjs
+        .sendForm(
+            'service_k0gqyht',
+            'template_buv38uz',
+            event.target,
+            'atXaPAG5EvldD1P_r'
+         )
+         .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at Samme1210@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+    document.body.classList.toggle("modal--open");
+}
